78bf27201944052b67adf0bb97f016853474896b
Raf Theunis <raf.theunis@kbc.be>
2020-08-24 06:45:19 +0200

    Externalize properties with PropertySourcesPlaceholderConfigurer

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,24 +12,10 @@ public class SfgDiApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-        System.out.println(i18nController.sayHello());
-
-
-        System.out.println("------ Primary Bean");
         MyController myController = (MyController) ctx.getBean("myController");
-        System.out.println(myController.sayHello());
 
-        System.out.println("------ Property");
-        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-        System.out.println(propertyInjectedController.getGreeting());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 
-        System.out.println("------ Setter");
-        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-        System.out.println(setterInjectedController.getGreeting());
-
-        System.out.println("------ Constructor");
-        ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-        System.out.println(constructorInjectedController.getGreeting());
+        System.out.println(fakeDataSource.getUser());
     }
 }
